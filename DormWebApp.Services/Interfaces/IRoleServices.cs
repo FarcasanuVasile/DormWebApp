ba666b4afe2605931c738aca5de488f0bb0f6d63
PrimeTech-03 <devop@PRIMETECH-03>
2019-03-12 15:38:25 +0200

    Added RoleServices

---
+++ DormWebApp.Services/Interfaces/IRoleServices.cs
@@ -0,0 +1,17 @@
+﻿using DormWebApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormWebApp.Services.Interfaces
+{
+    interface IRoleServices
+    {
+        void AddRole(Role role);
+        void EditRole(Role role);
+        void DeleteRole(Role role);
+        bool ExistingRole(string name);
+    }
+}
---
+++ DormWebApp.Services/Services/RoleServices.cs
@@ -0,0 +1,48 @@
+﻿using DormWebApp.Data.Infrastructure;
+using DormWebApp.Domain.Entities;
+using DormWebApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormWebApp.Services.Services
+{
+    public class RoleServices : IRoleServices
+    {
+        private DatabaseFactory dbFactory ;
+        private IUnitOfWork unitOfWork;
+        private Repository<Role> roleRepository;
+
+        public RoleServices( DatabaseFactory databaseFactory)
+        {
+            this.dbFactory = databaseFactory;
+            this.unitOfWork = new UnitOfWork(dbFactory);
+            this.roleRepository = new Repository<Role>(dbFactory);
+        }
+
+        public void AddRole(Role role)
+        {
+
+        }
+
+        public void EditRole(Role role)
+        {
+
+        }
+
+        public void DeleteRole(Role role)
+        {
+
+        }
+
+        public bool ExistingRole(string roleName)
+        {
+            return roleRepository.GetAll().Count(x => x.Name.ToLower().Replace(" ", string.Empty) == roleName.ToLower().Replace(" ", string.Empty)) > 0;
+        }
+
+        
+
+    }
+}
